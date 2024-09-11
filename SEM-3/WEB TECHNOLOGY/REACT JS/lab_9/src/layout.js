b1e292ebcb5040b07b2ff55411536941f6dd8411
RudraC-07 <rudrajchavda05@gmail.com>
2024-09-11 09:28:31 +0530

    added

--- SEM-3/WEB TECHNOLOGY/REACT JS/lab_9/src/layout.js
+++ SEM-3/WEB TECHNOLOGY/REACT JS/lab_9/src/layout.js
@@ -6,7 +6,7 @@ function Layout() {
       <div className='container'>
 
         <div className='row'>
-          <div className='border border-dark col-3'>LOGO</div>
+          <div className='border border-dark col-2'>LOGO</div>
           <div className='border border-primary col'>
             <Link to="/home" className='m-3'>Home</Link> &nbsp;
             <Link to="/about" className='m-3'>About</Link> &nbsp;
@@ -15,12 +15,12 @@ function Layout() {
         </div>
 
         <div className='row'>
-          <div className='col-4 border border-warning col-1'>Sidebar</div>
-          <div className='col border border-danger'><Outlet/></div>
+          <div className='col-3 border border-warning col-1 mt-2'>Sidebar</div>
+          <div className='col border border-danger mt-2'><Outlet/></div>
         </div>
 
         <div className='row'>
-          <div className='border border-dark'>Footer</div>
+          <div className='border border-dark mt-2'>Footer</div>
         </div>
 
       </div>
